4cbf4610bbebe4000c5cfadc09d6416cad9aeb99
Eric Bidelman <ebidel@gmail.com>
2012-05-04 16:20:36 -0700

    Ember completely gone

--- FinalProject/js/app.js
+++ FinalProject/js/app.js
@@ -1,4 +1,7 @@
 var wReader = angular.module('wReader', ['wReader.filters']);
+  // config(['$locationProvider', function($locationProvider) {
+  //   $locationProvider.html5Mode(true).hashPrefix('!');
+  // }]);
   // config(['$routeProvider', function($routeProvider) {
   //   //$routeProvider.when('/view1', {template: 'partials/partial1.html', controller: MyCtrl1});
   //   //$routeProvider.when('/view2', {template: 'partials/partial2.html', controller: MyCtrl2});
@@ -36,7 +39,6 @@ function Item() {
   this.read = false;
   this.starred = false;
   this.selected = false;
-
   this.item_id = null;
   this.title = null;
   this.pub_name = null;
@@ -48,34 +50,10 @@ function Item() {
   this.item_link = null;
 }
 
-// Create the all up Ember application
-var WReader = Em.Application.create({
-  ready: function() {
-    // Call the superclass's `ready` method.
-    this._super();
-
-    //On mobile devices, hide the address bar
-    window.scrollTo(0);
-
-    // Load items from the local data store first
-    //WReader.GetItemsFromDataStore();
-    //DataController.prototype.getItemsFromDataStore();
-  }
-});
-
-function DataController($scope, $http) {
+function DataController($scope, $http, $filter) {
   $scope.items = [];
-
-  // this.$scope = $scope;
-  // this.$http = $http;
-
-  // $scope.getItemsFromDataStore = function() {
-  //   return this.getItemsFromDataStore();
-  // }.bind(this);
-
-  // $scope.getItemsFromServer = function() {
-  //   return this.getItemsFromServer();
-  // }.bind(this);
+  $scope.filteredItems = [];
+  $scope.currentFilter = {};
 
   $scope.getItemsFromDataStore = function() {
     // Get all items from the local data store.
@@ -88,19 +66,12 @@ function DataController($scope, $http) {
         var item = new Item();
         angular.extend(item, entry);
         $scope.addItem(item);
-
-        //var dataController = new DataController({});
-        //dataController.addItem(item);
       });
 
       console.log("Entries loaded from local data store:", arr.length);
 
-      // Set the default view to any unread items
-      // WReader.itemsController.showDefault();
-
-
-      // Load items from the server after we've loaded everything from
-      //  the local data store
+      // Load items from the server after we've loaded everything from the local
+      // data store
       $scope.getItemsFromServer();
     });
   };
@@ -149,13 +120,9 @@ function DataController($scope, $http) {
           store.save(item);
         }
       });
-
-      // Refresh the visible items
-      //ItemsController.showDefault();
-      //var itemsController = new ItemsController({});
-      //itemsController.showDefault();
     };
 
+    $scope.items = [];
     $http.jsonp(feedURL + '&callback=JSON_CALLBACK').success(successCallback);
   };
 
@@ -196,20 +163,16 @@ function DataController($scope, $http) {
     return mid;
   };
 
-  // A 'property' that returns the count of items
   $scope.itemCount = function() {
     return $scope.items.length;
   };
 
-  // A 'property' that returns the count of read items
   $scope.readCount = function() {
     return $scope.items.filter(function(val, i) { return val.read }).length;
   };
 
-  // A 'property' that returns the count of unread items
   $scope.unreadCount = function() {
-    return $scope.items.filter(function(val, i) { return !val.read }).length;
-    // TODO use $scope.items.length - $scope.readCount() instead.
+    return $scope.items.length - $scope.readCount();
   };
 
   // A 'property' that returns the count of starred items
@@ -226,20 +189,58 @@ function DataController($scope, $http) {
     });
   };
 
+  $scope.clearFilter = function() {
+    $scope.filteredItems = $scope.items;
+  };
+
+  $scope.$watch('items', function(newVal, oldVal, scope) {
+    $scope.filterBy($scope.currentFilter.key, $scope.currentFilter.value);
+  }, true);
+
+  $scope.filterBy = function(key, value) {
+    // $scope.filteredItems = $scope.items.filter(function(val, i) {
+    //   return val[key] == value;
+    // });
+$scope.currentFilter = {key: key, value: value};
+
+    $scope.filteredItems = $filter('filter')($scope.items, function(item, i) {
+      return item[key] == value;
+    });
+  };
+
   // Fetch items when the constructor is called.
   $scope.getItemsFromServer();
 }
+DataController.$inject = ['$scope', '$http', '$filter']; // For JS compilers.
+
 
 function ItemsController($scope) { //{, $injector) {//, myService) {
   //$injector.invoke(DataController, this, {$scope: $scope}); 
 
   $scope.selectedItemIdx = null;
+
+  // A special observer that will watch for when the 'selectedItem' is updated
+  // and ensure that we scroll into a view so that the selected item is visible
+  // in the summary list view.
+  $scope.$watch('selectedItemIdx', function(newVal, oldVal, scope) {
+
+    // Performing scrolling like this isn't very Angular-ly.
+    // Need the setTimeout to prevent race condition with summary being selected
+    // after selectedItemIdx has already changed.
+    window.setTimeout(function() {
+      if (newVal != null) {
+        var curScrollPos = $('.summaries').scrollTop();
+        var itemTop = $('.summary.active').offset().top - 60;
+        $('.summaries').animate({'scrollTop': curScrollPos + itemTop}, 200);
+      }
+    }, 0);    
+  });
   
   $scope.hasNext = function() {
     if ($scope.selectedItemIdx == null) {
       return true;
     }
-    return $scope.selectedItemIdx < $scope.$parent.items.length - 1;
+    return $scope.selectedItemIdx < $scope.$parent.filteredItems.length - 1;
   };
   
   $scope.hasPrev = function() {
@@ -247,29 +248,6 @@ function ItemsController($scope) { //{, $injector) {//, myService) {
       return false;
     }
     return $scope.selectedItemIdx > 0;
-  };
-
-  // Sets content[] to the filtered results of the data controller
-  $scope.filterBy = function(key, value) {
-    //this.set('content', WReader.dataController.filterProperty(key, value));
-    return $scope.items.filter(function(val, i) {
-      return val[key] == value;
-    });
-  };
-
-  // Sets content[] to all items in the data controller
-  $scope.clearFilter = function() {
-    //this.set('content', WReader.dataController.get('content'));
-  };
-
-  // Shortcut for filterBy
-  $scope.showDefault = function() {
-    //$scope.filterBy('read', false);
-  };
-
-  // Mark all visible items read
-  $scope.markAllRead = function() {
-    $scope.$parent.markAllRead();
   };
 
   $scope.selectedItem = function() {
@@ -298,8 +276,6 @@ function ItemsController($scope) { //{, $injector) {//, myService) {
     }
 
     $scope.toggleRead(true);
-    //$scope.hasNext = !!this.$$nextSibling;
-    //$scope.hasPrev = !!this.$$prevSibling;
 
     //TODO: Update the address bar
     //var url = location.origin + location.pathname + '';
@@ -337,179 +313,10 @@ function ItemsController($scope) { //{, $injector) {//, myService) {
     store.toggleStar(key, star);
   };
 }
-//ItemsController.$inject = ['$scope', '$injector', '$filter'];
+ItemsController.$inject = ['$scope'];  // For JS compilers.
 //ItemsController.prototype = Object.create(DataController.prototype);
 
-// // Selected Item Controller - and provides functionality to hook into
-// // all details for a specific item.
-// WReader.selectedItemController = Em.Object.create({
-//   // Pointer to the seclected item
-//   selectedItem: null,
-
-//   hasPrev: false,
-
-//   hasNext: false,
-
-//   // Called to select an item
-//   select: function(item) {
-//     this.set('selectedItem', item);
-//     if (item) {
-//       this.toggleRead(true);
-
-//       // Determine if we have a previous/next item in the array
-//       var currentIndex = WReader.itemsController.content.indexOf(this.get('selectedItem'));
-//       if (currentIndex + 1 >= WReader.itemsController.get('itemCount')) {
-//         this.set('hasNext', false);
-//       } else {
-//         this.set('hasNext', true);
-//       }
-//       if (currentIndex === 0) {
-//         this.set('hasPrev', false);
-//       } else {
-//         this.set('hasPrev', true);
-//       }
-
-//       //TODO: Update the address bar
-//       //var url = location.origin + location.pathname + '';
-//       //var item_url = "" + item.get('item_id');
-//       //history.pushState(item.get('item_id'), 'title', url + item_url);
-
-//     } else {
-//       this.set('hasPrev', false);
-//       this.set('hasNext', false);
-//     }
-//   },
-
-//   // Toggles or sets the read state with an optional boolean
-//   toggleRead: function(read) {
-//     if (read === undefined) {
-//       read = !this.selectedItem.get('read');
-//     }
-//     this.selectedItem.set('read', read);
-//     var key = this.selectedItem.get('item_id');
-//     store.toggleRead(key, read);
-//   },
-
-//   // Toggles or sets the starred status with an optional boolean
-//   toggleStar: function(star) {
-//     if (star === undefined) {
-//       star = !this.selectedItem.get('starred');
-//     }
-//     this.selectedItem.set('starred', star);
-//     var key = this.selectedItem.get('item_id');
-//     store.toggleStar(key, star);
-//   },
-
-//   // Selects the next item in the item controller
-//   next: function() {
-//     // Get's the current index in case we've changed the list of items, if the
-//     // item is no longer visible, it will return -1.
-//     var currentIndex = WReader.itemsController.content.indexOf(this.get('selectedItem'));
-//     // Figure out the next item by adding 1, which will put it at the start
-//     // of the newly selected items if they've changed.
-//     var nextItem = WReader.itemsController.content[currentIndex + 1];
-//     if (nextItem) {
-//       this.select(nextItem);
-//     }
-//   },
-
-//   // Selects the previous item in the item controller
-//   prev: function() {
-//     // Get's the current index in case we've changed the list of items, if the
-//     // item is no longer visible, it will return -1.
-//     var currentIndex = WReader.itemsController.content.indexOf(this.get('selectedItem'));
-//     // Figure out the previous item by subtracting 1, which will result in an
-//     // item not found if we're already at 0
-//     var prevItem = WReader.itemsController.content[currentIndex - 1];
-//     if (prevItem) {
-//       this.select(prevItem);
-//     }
-//   }
-// });
-
-
-// A special observer that will watch for when the 'selectedItem' is updated
-// and ensure that we scroll into a view so that the selected item is visible
-// in the summary list view.
-WReader.selectedItemController.addObserver('selectedItem', function() {
-  var curScrollPos = $('.summaries').scrollTop();
-  var itemTop = $('.summary.active').offset().top - 60;
-  $(".summaries").animate({"scrollTop": curScrollPos + itemTop}, 200);
-});
-
-// View for the ItemsList
-WReader.SummaryListView = Em.View.extend({
-  tagName: 'article',
-
-  classNames: ['well', 'summary'],
-
-  classNameBindings: ['active', 'read', 'prev', 'next'],
-
-  // Handle clicks on item summaries with the same code path that
-  // handles the touch events.
-  click: function(evt) {
-    this.touchEnd(evt);
-  },
-
-  // Handle clicks/touch/taps on an item summary
-  touchEnd: function(evt) {
-    // Figure out what the user just clicked on, then set selectedItemController
-    var content = this.get('content');
-    WReader.selectedItemController.select(content);
-  },
-
-  // Enables/Disables the active CSS class
-  active: function() {
-    var selectedItem = WReader.selectedItemController.get('selectedItem');
-    var content = this.get('content');
-    if (content === selectedItem) {
-      return true;
-    }
-  }.property('WReader.selectedItemController.selectedItem'),
-
-  // Enables/Disables the read CSS class
-  read: function() {
-    var read = this.get('content').get('read');
-    return read;
-  }.property('WReader.itemsController.@each.read'),
-
-  // Returns the date in a human readable format
-  formattedDate: function() {
-    var d = this.get('content').get('pub_date');
-    return moment(d).fromNow();
-  }.property('WReader.selectedItemController.selectedItem')
-});
-
-
-// // View for the Selected Item
-// WReader.EntryItemView = Em.View.extend({
-//   tagName: 'article',
-
-//   contentBinding: 'WReader.selectedItemController.selectedItem',
-
-//   classNames: ['well', 'entry'],
-
-//   classNameBindings: ['active', 'read', 'prev', 'next'],
-
-//   // Enables/Disables the active CSS class
-//   active: function() {
-//     return true;
-//   }.property('WReader.selectedItemController.selectedItem'),
-
-//   // Enables/Disables the read CSS class
-//   read: function() {
-//     var read = this.get('content').get('read');
-//     return read;
-//   }.property('WReader.itemsController.@each.read'),
-
-//   // Returns a human readable date
-//   formattedDate: function() {
-//     var d = this.get('content').get('pub_date');
-//     return moment(d).format("MMMM Do YYYY, h:mm a");
-//   }.property('WReader.selectedItemController.selectedItem')
-// });
-
-// Top Menu/Nav Bar view
+// Top Menu/Nav Bar
 function NavBarController($scope) {//, $injector) {
   //$injector.invoke(ItemsController, this, {$scope: $scope}); 
 
@@ -530,99 +337,21 @@ function NavBarController($scope) {//, $injector) {
 
   // Click handler for menu bar
   $scope.showRead = function() {
-    $scope.filterBy('read', true);
+    $scope.$parent.filterBy('read', true);
   };
 
   // Click handler for menu bar
   $scope.refresh = function() {
     $scope.getItemsFromServer();
   };
+
+  $scope.showAll(); // Show all when page loads.
 }
-// //NavBarController.$inject = ['$scope', '$injector'];
+
+NavBarController.$inject = ['$scope'];  // For JS compilers.
 // NavBarController.prototype = Object.create(ItemsController.prototype);
 
-// Left hand controls view
-// WReader.NavControlsView = Em.View.extend({
-//   tagName: 'section',
-
-//   classNames: ['controls'],
-
-//   classNameBindings: ['hide'],
-
-//   hide: function() {
-//     return false;
-//   }.property('WReader.settingsController.tabletControls'),
-
-//   // Click handler for up/previous button
-//   navUp: function(e) {
-//     WReader.selectedItemController.prev();
-//   },
-
-//   // Click handler for down/next button
-//   navDown: function(e) {
-//     WReader.selectedItemController.next();
-//   },
-
-//   // Click handler to toggle the selected items star status
-//   toggleStar: function(e) {
-//     WReader.selectedItemController.toggleStar();
-//   },
-
-//   // Click handler to toggle the selected items read status
-//   toggleRead: function(e) {
-//     WReader.selectedItemController.toggleRead();
-//   },
-
-//   // Click handler to mark all as read
-//   markAllRead: function(e) {
-//     WReader.itemsController.markAllRead();
-//   },
-
-//   // Click handler for refresh
-//   refresh: function(e) {
-//     WReader.GetItemsFromServer();
-//   },
-
-
-//   starClass: function() {
-//     var selectedItem = WReader.selectedItemController.get('selectedItem');
-//     if (selectedItem) {
-//       if (selectedItem.get('starred')) {
-//         return 'icon-star';
-//       }
-//     }
-//     return 'icon-star-empty';
-//   }.property('WReader.selectedItemController.selectedItem.starred'),
-//   readClass: function() {
-//     var selectedItem = WReader.selectedItemController.get('selectedItem');
-//     if (selectedItem) {
-//       if (selectedItem.get('read')) {
-//         return 'icon-ok-sign';
-//       }
-//     }
-//     return 'icon-ok-circle';
-//   }.property('WReader.selectedItemController.selectedItem.read'),
-//   nextDisabled: function() {
-//     return !WReader.selectedItemController.get('hasNext');
-//   }.property('WReader.selectedItemController.selectedItem.next'),
-//   prevDisabled: function() {
-//     return !WReader.selectedItemController.get('hasPrev');
-//   }.property('WReader.selectedItemController.selectedItem.prev'),
-//   buttonDisabled: function() {
-//     var selectedItem = WReader.selectedItemController.get('selectedItem');
-//     if (selectedItem) {
-//       return false;
-//     }
-//     return true;
-//   }.property('WReader.selectedItemController.selectedItem')
-// });
-
 function NavControlsView($scope) {
-  $scope.tabletControls = false;
-
-  $scope.hide = function() {
-    return false;
-  };
 
   // Click handler for up/previous button
   $scope.navUp = function() {
@@ -650,51 +379,13 @@ function NavControlsView($scope) {
   };
 
   // Click handler for refresh
-  $scope.refresh = function(e) {
-    //WReader.GetItemsFromServer();
-    console.log('NavControlsView')
+  $scope.refresh = function() {
+    $scope.getItemsFromServer();
   };
-
-  $scope.starClass = function() {
-    var selectedItem = WReader.selectedItemController.get('selectedItem');
-    if (selectedItem) {
-      if (selectedItem.get('starred')) {
-        return 'icon-star';
-      }
-    }
-    return 'icon-star-empty';
-  };//.property('WReader.selectedItemController.selectedItem.starred'),
-  
-  $scope.readClass = function() {
-    var selectedItem = WReader.selectedItemController.get('selectedItem');
-    if (selectedItem) {
-      if (selectedItem.get('read')) {
-        return 'icon-ok-sign';
-      }
-    }
-    return 'icon-ok-circle';
-  };//.property('WReader.selectedItemController.selectedItem.read'),
-  
-  $scope.nextDisabled = function() {
-    return !WReader.selectedItemController.get('hasNext');
-  };//.property('WReader.selectedItemController.selectedItem.next'),
-  
-  $scope.prevDisabled = function() {
-    return !WReader.selectedItemController.get('hasPrev');
-  };//.property('WReader.selectedItemController.selectedItem.prev'),
-  
-  $scope.buttonDisabled = function() {
-    var selectedItem = WReader.selectedItemController.get('selectedItem');
-    if (selectedItem) {
-      return false;
-    }
-    return true;
-  };//.property('WReader.selectedItemController.selectedItem')
 }
 
 
-
-WReader.HandleSpaceKey = function() {
+function handleSpace() {
   var itemHeight = $('.entry.active').height() + 60;
   var winHeight = $(window).height();
   var curScroll = $('.entries').scrollTop();
@@ -702,7 +393,7 @@ WReader.HandleSpaceKey = function() {
   if (scroll < itemHeight) {
     $('.entries').scrollTop(scroll);
   } else {
-    WReader.selectedItemController.next();
+    $('.controls button[ng-click="navDown()"]').click();
   }
 };
 
@@ -712,11 +403,11 @@ function handleBodyKeyDown(e) {
     case 39: // right arrow
     case 40: // down arrow
     case 74: // j
-      WReader.selectedItemController.next();
+      $('.controls button[ng-click="navDown()"]').click(); // TODO: figure out angular way
       break;
 
     case 32: // Space
-      WReader.HandleSpaceKey();
+      handleSpace();
       e.preventDefault();
       break;
 
@@ -724,15 +415,15 @@ function handleBodyKeyDown(e) {
     case 37: // left arrow
     case 38: // up arrow
     case 75: // k
-      WReader.selectedItemController.prev();
+      $('.controls button[ng-click="navUp()"]').click();
       break;
 
     case 85: // U
-      WReader.selectedItemController.toggleRead();
+      $('.controls button[ng-click="toggleRead()"]').click();
       break;
 
     case 72: // H
-      WReader.selectedItemController.toggleStar();
+      $('.controls button[ng-click="toggleStar()"]').click();
       break;
   }
 }
@@ -743,3 +434,8 @@ function handlePopState(e) {
 
 document.body.addEventListener('keydown', handleBodyKeyDown, false);
 window.addEventListener('popstate', handlePopState, false);
+
+document.addEventListener('DOMContentLoaded', function(e) {
+  //On mobile devices, hide the address bar
+  window.scrollTo(0);
+}, false);
